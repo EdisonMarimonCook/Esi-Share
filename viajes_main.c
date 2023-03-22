1573ea4401a3fea63c027580aee34feb98c906b8
CLAUDICHAN <127423287+CLAUDICHAN@users.noreply.github.com>
2023-03-22 21:28:24 +0100

    Update viajes_main.c

--- viajes_main.c
+++ viajes_main.c
@@ -3,41 +3,16 @@
 #include <locale.h>
 #include <string.h>
 #include "viajes_header.h"
+#define N 100
 
-void subir_viaje(){
+void subir_viaje(FILE *viajes_file){
     estruct_viajes Id_m;
-    printf("°Bienvenido al creador de viajes de ESI Share!\n\nIntroduzca la matrÌcula del coche que vaya a utilizar: ");
+    printf("¬°Bienvenido al creador de viajes de ESI Share!\n\nIntroduzca la matr√≠cula del coche que vaya a utilizar: ");
     /*fflush(stdin);
     fgets(v.Id_mat, sizeof(char), stdin); PROBLEMA. No te pide escribir la cadena de caracteres*/
 }
 
-int power (int base, int exp){
-    int i, res=1;
-    for(i=exp;i>0;i--){
-        res*=base;
-    }
-    return res;
-}
-
-void crear_vector(int n, int *vector){
-    //DeclaraciÛn de datos:
-    int i, j=0;
-    //Crea el vector. Intoduce dÌgito a dÌgito el ID en cada celda:
-    for(i=5;i>=0;i--){
-        *(vector+j)=n/power(10,i);
-        n=n%power(10,i);
-        j++;
-    }
-}
-
-void escribir_vector (int *vector){
-    int i;
-    for(i=0;i<6;i++){
-        printf("%d", *(vector+i));
-    }
-}
-
-int contar (int n){     //arreglar que no cuenta 000001 como 6 dÌgitos
+int contar (int n){     //arreglar que no cuenta 000001 como 6 d√≠gitos
     int res=0;
     while(n>0){
         n/=10;
@@ -46,113 +21,152 @@ int contar (int n){     //arreglar que no cuenta 000001 como 6 dÌgitos
     return res;
 }
 
+void vaciar_vector (char vector[]){
+
+    //Declaraci√≥n de datos
+    int i;
+
+
+    //Introduzco en valor '\0' en todas las celdas del vector para vaciarlo:
+    for(i=0;i<100;i++){
+        vector[i]='\0';
+    }
+}
+
 void eliminar_viaje(FILE *viajes_file){
-    //DeclaraciÛn de datos:
-    char c, vector_aux[6], vector_aux_grande[100];
-    int id, vector_id[6], vector_fichero[6], i, cont=0, lineas_total=0, lineas_aux, lineas_cont=1, encontrado=0;
-    //EnseÒa por pantalla todos los viajes guardados en el fichero:
-    printf("øQuÈ viaje desea eliminar?\n\n");
+
+    //Declaraci√≥n de datos:
+    char aux, vector_temporal[N];
+    int id=0, id_fichero=0, i, encontrado=0, lineas=1, lineas_total=0;
+
+
+    //Ense√±a por pantalla todos los viajes guardados en el fichero:
+    printf("¬øQu√© viaje desea eliminar?\n\n");
+    while(!feof(viajes_file)){
+        fgets(vector_temporal,N,viajes_file);
+        printf("%s", vector_temporal);
+        lineas_total++;
+    }
+
+
+    //Creo una variable para el ID:
+    printf("\n\nIntroduzca el ID del viaje a borrar: ");
+    scanf("%d", &id);
+
+
+    /*//Comprobaci√≥n de que el ID dado es v√°lido en n√∫mero de d√≠gitos: ##################NO FUNCIONA BN, EJ:000001==1#################
+    while(contar(id)!=6){
+        printf("ID inv√°lido, el ID debe de contener 6 d√≠gitos, por favor int√©ntelo de nuevo: ");
+        scanf("%d", &id);
+    }*/
+
+
+    //Crear ID obtenida del fichero:
+    rewind(viajes_file); //Pone el puntero en la posici√≥n inicial del fichero
+    vaciar_vector(vector_temporal);  //Quitamos la basura de este vector
+    for(i=0;aux!='-';i++){
+        aux=fgetc(viajes_file);
+        vector_temporal[i]=aux;
+    }
+    id_fichero=atoi(vector_temporal);  //La funci√≥n atoi transforma los valores char del vector en un entero
+
+
+    //Comparaci√≥n:
+    do{
+        if(id==id_fichero){
+            encontrado=1;
+        }else{
+            aux='0';
+            while(aux!='\n'){
+                aux=fgetc(viajes_file);
+            }
+            vaciar_vector(vector_temporal);
+            for(i=0;aux!='-';i++){
+                aux=fgetc(viajes_file);
+                vector_temporal[i]=aux;
+            }
+            id_fichero=atoi(vector_temporal);
+            lineas;
+        }
+    }while(lineas!=lineas_total&&encontrado==0);
+
+    if(encontrado==1){
+        printf("\nEl viaje ha sido encontrado");
+    }else{
+        printf("\nNo se ha encontrado el viaje en la base de datos");
+    }
+
+    //Borrar:*/
+}
+
+void modificar_viaje(FILE *viajes_file){
+    char c;
+    printf("¬øQu√© viaje desea modificar?\n\n");
+    //Ense√±a por pantalla todos los viajes guardados en el fichero:
     do{
         c=fgetc(viajes_file);
         printf("%c", c);
     }while(c!=EOF);
-    //Crea un vector con cada dÌgito del ID en sus 6 celdas:
-    printf("\nIntroduzca el ID del viaje a borrar: ");
-    scanf("%d", &id);
-    /*//ComprobaciÛn de que el ID dado es v·lido en n˙mero de dÌgitos: ##################NO FUNCIONA BN, EJ:000001==1#################
-    while(contar(id)!=6){
-        printf("ID inv·lido, el ID debe de contener 6 dÌgitos, por favor intÈntelo de nuevo: ");
-        scanf("%d", &id);
-    }*/
-    crear_vector(id, vector_id);
-    escribir_vector(vector_id);
-    //#############################PROBLEMAS#########################################
-    //Crear vector de fichero:
-    printf("\n");
-    for(i=0;i<5;i++){
-        vector_fichero[i]=fgetc(viajes_file);
-    }
-    escribir_vector(vector_fichero);
-    /*//ComparaciÛn:
-    do{
-        i=0;
-        while(i<6||cont==0){
-            if(vector_fichero[i]!=vector_id[i]){
-                cont++;
-            }
-            i++;
-        }
-        if(cont==0){
-            printf("\nEL VIAJE EXISTE");
-            encontrado=1;
-        }else{
-            lineas_aux=0;
-            while(lineas_aux!=lineas_cont){
-                while(c!='\n'){
-                    c=fgetc(viajes_file);
-                }
-                lineas_aux++;
-            }
-            lineas_cont++;
-            printf("\n");
-            fgets(vector_aux,6,viajes_file);
-            id=atoi(vector_aux);
-            crear_vector(id, vector_fichero);
-        }
-    }while(lineas_cont!=lineas_total||encontrado==0);
-    //Borrar:*/
-}
-
-void modificar_viaje(){
-    printf("øQuÈ viaje desea modificar?");
 }
 
 void menu_viajes(){
-    //DeclaraciÛn de datos:
+
+    //Declaraci√≥n de datos:
     int elect, cont_menu=0;
     FILE *viajes_file;
     char res;
-    //Men˙ gr·fico:
+
+
+    //Men√∫ gr√°fico:
     do{
         printf("##       ##   ##      ###       ########  ######   ######\n");
         printf(" ##     ##    ##     ## ##         ##     ##      ##\n");
         printf("  ##   ##     ##    ##   ##        ##     ######   ######\n");
         printf("   ## ##      ##   #########   ##  ##     ##            ##\n");
         printf("    ###       ##  ##       ##   ####      ######   ######\n");
-        printf("\n #######################");
-        printf("\n |øQuÈ desea hacer?    |");
-        printf("\n |                     |");
-        printf("\n |1. AÒadir un viaje   |\n |2. Eliminar un viaje |\n |3. Modificar un viaje|\n");
-        printf(" #######################\n\n");
+        printf("\n ########################");
+        printf("\n |¬øQu√© desea hacer?     |");
+        printf("\n |                      |");
+        printf("\n |1. A√±adir un viaje    |\n |2. Eliminar un viaje  |\n |3. Modificar un viaje |\n |4. Salir              |\n");
+        printf(" ########################\n\n");
         scanf("%d", &elect);
-        //ComprobaciÛn de elecciÛn de men˙ v·lida:
-        while(elect<1||elect>3){
-            printf("VALOR INV¡LIDO: Introduzca '1', '2' o '3' para seleccionar la opciÛn deseada\n");
+
+
+        //Comprobaci√≥n de elecci√≥n de men√∫ v√°lida:
+        while(elect<1||elect>4){
+            printf("VALOR INV√ÅLIDO: Introduzca '1', '2', '3' o '4' para seleccionar la opci√≥n deseada\n");
             scanf("%d", &elect);
         }
         system ("cls"); //Sirve para limpiar la pantalla
-        //Abro el fichero viajes_file.txt antes de elegir quÈ hacer con Èl para no tener que abrirlo en cada funciÛn:
+
+
+        //Abro el fichero viajes_file.txt antes de elegir qu√© hacer con √©l para no tener que abrirlo en cada funci√≥n:
         viajes_file=fopen("viajes_file.txt", "a+");
         if(viajes_file!=NULL){
             switch (elect){
-                case 1: subir_viaje();
+                case 1: subir_viaje(viajes_file);
                 break;
                 case 2: eliminar_viaje(viajes_file);
                 break;
-                case 3: modificar_viaje();
+                case 3: modificar_viaje(viajes_file);
+                break;
+                case 4: exit(1);
             }
         }else{
             //Error dado en el caso de no poder abrir el fichero:
             printf("######################################################################\n");
-            printf("|Ha ocurrido un error a la hora de abrir el fichero <viajes_file.txt>|\n|    Por favor, intÈntelo m·s tarde o consulte con mantenimiento     |\n");
+            printf("|Ha ocurrido un error a la hora de abrir el fichero <viajes_file.txt>|\n|    Por favor, int√©ntelo m√°s tarde o consulte con mantenimiento     |\n");
             printf("######################################################################\n\n");
+            exit(1);
         }
         fclose(viajes_file);
-        //ContinuaciÛn del bucle??:
-        printf("\n\nøDesea continuar modificando sus viajes?\n(s/n)\n");
+
+
+        //Elecci√≥n sobre continuaci√≥n del bucle:
+        printf("\n\n¬øDesea continuar modificando sus viajes?\n(s/n)\n");
         scanf(" %c", &res);
         while(res!='s'&&res!='n'&&res!='N'&&res!='S'){
-            printf("VALOR INV¡LIDO: Responda con 's' para continuar o 'n' para salir del men˙ viajes\n");
+            printf("VALOR INV√ÅLIDO: Responda con 's' para continuar o 'n' para salir del men√∫ viajes\n");
             scanf(" %c", &res);
         }
         system ("cls");
@@ -163,7 +177,7 @@ void menu_viajes(){
 }
 
 int main(){
-    setlocale(LC_ALL, "spanish");
+    setlocale(LC_ALL, "spanish");  //Funci√≥n para poder escribir car√°cteres espec√≠ficos del espa√±ol
     menu_viajes();
     return 0;
 }
